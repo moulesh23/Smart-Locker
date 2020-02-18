e420a4b705efe9b1c09beb6f28ca77fc139ec41c
MOULESH <moulesh23@gmail.com>
2020-02-18 11:18:57 +0530

    Add files via upload

---
+++ Smart_Locker.ino
@@ -0,0 +1,83 @@
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "858d4d61-ec4f-433a-9022-02e7f3d66ff5"
+#define CHARACTERISTIC_UUID "51fe2520-5bfb-496d-bbb5-b7326c634f41"
+#define LED 2
+class MyCallbacks: public BLECharacteristicCallbacks 
+{
+    void onWrite(BLECharacteristic *pCharacteristic) 
+    {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        Serial.println("*********");
+        Serial.print("New value:");
+        for (int i = 0; i < value.length(); i++)
+          Serial.print(value[i]);
+
+        Serial.println();
+        Serial.println("*********");
+      }
+      if(value == "a")
+      {
+        Serial.println("Turning ON the led");
+        digitalWrite(LED,HIGH);
+        delay(500);
+        digitalWrite(LED,LOW);
+      }
+       else
+       {
+        Serial.println("else");
+          for (int i = 0; i < value.length(); i++)
+          Serial.print(value[i]);
+       }
+        
+        
+      }
+     
+
+
+};
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);
+
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to MyESP32");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+
+  BLEDevice::init("MyESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+}
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
